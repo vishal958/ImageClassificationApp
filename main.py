56f29659506410e47b5281d8dafcdcb696347fde
vishal958 <vishalmaurya958@gmail.com>
2020-08-02 18:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ def classify_image():
     return response
 
 if __name__ == "__main__":
-    #print("Starting Python Flask Server For Sports Celebrity Image Classification")
     util.load_saved_artifacts()
     app.run()
