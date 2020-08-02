ce7a9a5000e38bdc422bb0bc2bf5d4d3f817175c
Vishal Maurya <Vishal_Maurya@spe.sony.com>
2020-08-02 18:05:59 +0530

    Imageclassification added

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app.main import app 
+from app.util import load_saved_artifacts
   
-if __name__ == "__main__": 
-        app.run() 
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    util.load_saved_artifacts()
+    app.run(port=5000)
