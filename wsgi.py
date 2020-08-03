49066cb9f6900cce10ea1e4939ef92361b4bfee8
Vishal Maurya <Vishal_Maurya@spe.sony.com>
2020-08-03 09:32:44 +0530

    folder structure change

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
-from app.main import app 
-  
+from app.main import load_saved_artifacts
+from app.main import app
+# from app.wavelet import w2d
+# #from app.util import w2d
 
 if __name__ == "__main__": 
+        load_saved_artifacts()
         app.run() 
 
